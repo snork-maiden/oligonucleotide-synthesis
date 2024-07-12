0d02621357434f7295d3d4fe91467929d7adc06d
snork-maiden <pollykoldysheva@gmail.com>
2024-07-12 18:58:15 +0300

    Working sequency input with validation

--- src/components/TheSequenceInput.vue
+++ src/components/TheSequenceInput.vue
@@ -1,14 +1,42 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useSynthesizerStore } from "@/stores/synthesizer";
+import { ref, watch, type Ref } from "vue";
+const store = useSynthesizerStore();
+
+let sequence: Ref<string> = ref("");
+let isLengthError = ref(false);
+
+watch(sequence, (string) => {
+  sequence.value = string.replace(/[^atgc]/gi, "");
+});
+
+function add() {
+  if (sequence.value.length < 6 || sequence.value.length > 120) {
+    isLengthError.value = true;
+    return;
+  }
+  isLengthError.value = false;
+  store.addSequence(sequence.value);
+  sequence.value = "";
+}
+</script>
 
 <template>
-  <label class="sequence">
-    <span class="label">Введите последовательность</span>
-    <input type="text" class="sequence" />
-  </label>
-  <div class="warning">Длина от 6 до 120 символов</div>
   <div class="warning">Только символы a, t, g, c</div>
-  <button type="button" class="add">Добавить</button>
+  <form @submit.prevent="add">
+    <label class="sequence">
+      <span class="label">Введите последовательность</span>
+      <input
+        type="text"
+        class="sequence"
+        v-model="sequence"
+        minlength="6"
+        maxlength="120"
+      />
+    </label>
+    <button type="submit" class="add">Добавить</button>
+  </form>
+  <div class="warning" v-if="isLengthError">Длина от 6 до 120 символов</div>
 </template>
 
-<script scoped>
-</script>
+<style scoped></style>
