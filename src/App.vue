0a4ce239c71ecb8220898672a24f5db2296ebbf2
snork-maiden <pollykoldysheva@gmail.com>
2024-07-12 20:50:06 +0300

    fix command, add titles

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,12 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
+  <h1 class="title">Синтезатор олигонуклеотидов</h1>
   <RouterView />
 </template>
 
 <style scoped>
-
+.title {
+  text-align: center;
+}
 </style>
