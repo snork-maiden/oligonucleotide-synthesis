cca06f8db677e7a862f399dd6d1e86ba05e47ef6
snork-maiden <pollykoldysheva@gmail.com>
2024-07-12 13:35:33 +0300

    first commit

---
+++ src/components/TheMain.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <label class="sequence">
+    <span class="label">Введите последовательность</span>
+    <input type="text" class="sequence" />
+  </label>
+  <div class="warning">Длина от 6 до 120 символов</div>
+  <div class="warning">Только символы a, t, g, c</div>
+  <button type="button" class="add">Добавить</button>
+</template>
+
+<script scoped>
+</script>
